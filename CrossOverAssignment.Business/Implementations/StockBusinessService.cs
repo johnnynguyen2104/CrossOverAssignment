8c16d587753ab50c307531c81b67677c287ec370
Thuan <carrie.he1704@gmail.com>
2017-04-19 16:38:44 +0700

    create details for stock business service and applied DI pattern

--- CrossOverAssignment.Business/Implementations/StockBusinessService.cs
+++ CrossOverAssignment.Business/Implementations/StockBusinessService.cs
@@ -5,29 +5,83 @@ using System.Text;
 using System.Threading.Tasks;
 using CrossOverAssignment.Business.Dtos;
 using CrossOverAssignment.Business.Interfaces;
+using CrossOverAssignment.Business.StockWebService;
+using CrossOverAssignment.DAL.DomainModels;
+using CrossOverAssignment.DAL.Implementations;
+using CrossOverAssignment.DAL.Interfaces;
 
 namespace CrossOverAssignment.Business.Implementations
 {
     public class StockBusinessService : IStockBusinessService
     {
+        private readonly StockExchangeWebService stockExchangeWebService;
+
+        private readonly IBaseRepository<int, Stock> stockRepository;
+
+        public StockBusinessService(IBaseRepository<int, Stock> baseRepository)
+        {
+            stockRepository = baseRepository;
+        }
+
+        public StockBusinessService()
+        {
+            stockExchangeWebService = new StockExchangeWebService()
+            {
+                UserAuthValue = new UserAuth()
+                {
+                    UserName = "",
+                    Password = ""
+                }
+            };
+
+            stockRepository = new Repository<int, Stock>();
+        }
+
         public StockDto CreateStock(StockDto dto)
         {
-            throw new NotImplementedException();
+            Stock stock = new Stock() { StockCode = dto.StockCode, UserId = dto.UserId };
+
+            var result = stockRepository.Create(stock);
+
+            return stockRepository.CommitChanges() > 0
+                ? new StockDto()
+                {
+                    UserId = result.UserId,
+                    Id = result.Id,
+                    StockCode = result.StockCode,
+                    CreateDateTime = result.CreateDateTime,
+                    UpdateDateTime = result.UpdateDateTime
+                }
+                : null;
         }
 
         public int Delete(int id)
         {
-            throw new NotImplementedException();
+            stockRepository.Delete(a => a.Id == id);
+
+            return stockRepository.CommitChanges();
         }
 
         public int Delete(IList<int> ids)
         {
-            throw new NotImplementedException();
+            stockRepository.Delete(a => ids.Contains(a.Id));
+
+            return stockRepository.CommitChanges();
         }
 
         public IList<StockDto> ReadStocksByUser(string userId)
         {
-            throw new NotImplementedException();
+            var result = stockRepository.Read(a => a.UserId == userId)
+                                        .Select(a => new StockDto()
+                                        {
+                                            UserId = a.UserId,
+                                            Id = a.Id,
+                                            StockCode = a.StockCode,
+                                            CreateDateTime = a.CreateDateTime,
+                                            UpdateDateTime = a.UpdateDateTime
+                                        }).ToList();
+
+            return result;
         }
     }
 }
