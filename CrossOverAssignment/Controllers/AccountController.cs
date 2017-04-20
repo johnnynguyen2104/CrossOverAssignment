ca0f3414f225076a75ac0c8be0688be7ebc2cb28
Thuan <carrie.he1704@gmail.com>
2017-04-20 13:37:02 +0700

    update ui for login and register

--- CrossOverAssignment/Controllers/AccountController.cs
+++ CrossOverAssignment/Controllers/AccountController.cs
@@ -375,10 +375,6 @@ namespace CrossOverAssignment.Controllers
             return View(model);
         }
 
-        //
-        // POST: /Account/LogOff
-        [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
